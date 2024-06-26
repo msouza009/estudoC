ed8bb7f5dc98b38dbf908f32ab9e39603d6859e8
Matheus <szmatheussouza@gmail.com>
2024-06-26 00:07:18 -0300

    Inserido Arvore Binária

---
+++ ArvoreBinariaBusca/cmake-build-debug/CMakeFiles/ArvoreBinariaBusca.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ArvoreBinariaBusca"
+  "ArvoreBinariaBusca.pdb"
+  "CMakeFiles/ArvoreBinariaBusca.dir/main.c.o"
+  "CMakeFiles/ArvoreBinariaBusca.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ArvoreBinariaBusca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
