18a5bf04a11aa88f73fd744f9edd1196970dec8d
Matheus <szmatheussouza@gmail.com>
2024-06-25 23:31:31 -0300

    estudoC

---
+++ TabelaHashDivison/cmake-build-debug/CMakeFiles/TabelaHashDivison.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TabelaHashDivison.dir/main.c.o"
+  "CMakeFiles/TabelaHashDivison.dir/main.c.o.d"
+  "TabelaHashDivison"
+  "TabelaHashDivison.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TabelaHashDivison.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TabelaHashDivison/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TABLE_SIZE 10
+
