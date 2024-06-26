d791041d85ebb19cfe7acbf431f2cc250dffabc1
Matheus <szmatheussouza@gmail.com>
2024-06-26 00:12:54 -0300

    Inserido arvoreAVL

---
+++ ArvoreAvl/cmake-build-debug/CMakeFiles/ArvoreAvl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ArvoreAvl"
+  "ArvoreAvl.pdb"
+  "CMakeFiles/ArvoreAvl.dir/main.c.o"
+  "CMakeFiles/ArvoreAvl.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ArvoreAvl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
